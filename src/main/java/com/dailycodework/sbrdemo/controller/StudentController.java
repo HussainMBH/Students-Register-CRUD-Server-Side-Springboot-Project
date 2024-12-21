b7b97355d4e519b1a65802d7edfb833f39026d99
Bahir Hussain <mbahirh624@gmail.com>
2024-12-21 15:14:21 +0530

    update

--- src/main/java/com/dailycodework/sbrdemo/controller/StudentController.java
+++ src/main/java/com/dailycodework/sbrdemo/controller/StudentController.java
@@ -9,9 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-/**
- * @author Simpson Alfred
- */
+
 @CrossOrigin("http://localhost:3000") //allowing client application to consume the backed
 @RestController
 @RequestMapping("/students")
