73ecab2229cbbf204eb6e2a13a121e0fc8bc8d2b
Bahir Hussain <mbahirh624@gmail.com>
2024-12-22 11:55:30 +0530

    added

--- src/main/java/com/dailycodework/sbrdemo/exception/StudentNotFoundException.java
+++ src/main/java/com/dailycodework/sbrdemo/exception/StudentNotFoundException.java
@@ -1,8 +1,5 @@
 package com.dailycodework.sbrdemo.exception;
 
-/**
- * @author Simpson Alfred
- */
 
 public class StudentNotFoundException extends RuntimeException {
     public StudentNotFoundException(String message) {
